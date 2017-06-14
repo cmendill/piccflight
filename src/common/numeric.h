a8f8ac2039ef6cb89f257c85dbbbba338cde8e5d
Kuravi H <kuravih@gmail.com>
2017-06-14 15:43:24 -0400

    SHK zernike fitting working but numbers seem off

--- src/common/numeric.h
+++ src/common/numeric.h
@@ -1,3 +1,4 @@
 
-void num_dgemv(double *A, double *b, double *result, int m, int n);
-void num_dgesvdi(double* A, double* A_inv, int m, int n);
+void num_dgemv(double *A, double *b, double *result, int mm, int nn);
+void num_dgemm(double *A, double *B, double *result, int mm, int kk, int nn);
+void num_dgesvdi(double* A, double* A_inv, int mm, int nn);
--- src/shk/shk_functions.c
+++ src/shk/shk_functions.c
@@ -157,8 +157,8 @@ void shk_centroid(uint16 *image, shkcell_t *cells, sm_t *sm_p){
 /**************************************************************/
 /*                   SHK_ZERNIKE_MATRIX                       */
 /**************************************************************/
-void shk_zernike_matrix(shkcell_t *cells,double *matrix_inv){
-  int i, beam_ncells=0, beam_ncells_2=0;
+void shk_zernike_matrix(shkcell_t *cells, double *matrix_inv){
+  int i, beam_ncells=0;
   int beam_cell_index[SHK_NCELLS] = {0};
   double max_x = 0, max_y = 0, min_x = 0, min_y = 0;
   double temp_beam_center[2] = {0}, beam_center[2] = {0}, beam_radius[2]={0}, beam_radius_m[2]={0};
@@ -168,7 +168,6 @@ void shk_zernike_matrix(shkcell_t *cells,double *matrix_inv){
   // beam_radius = [(max(x)-min(x))/2, (max(y)-min(y))/2] of beam cell origins
   for(i=0; i<SHK_NCELLS; i++) {
     if (cells[i].beam_select) {
-      beam_ncells++;
       beam_cell_index[beam_ncells] = i;
       temp_beam_center[0] += cells[i].origin[0]; // center
       temp_beam_center[1] += cells[i].origin[1]; // center
@@ -176,9 +175,9 @@ void shk_zernike_matrix(shkcell_t *cells,double *matrix_inv){
       max_y = (cells[i].origin[1] > max_y) ? cells[i].origin[1] : max_y; // hold on to max y
       min_x = (cells[i].origin[0] < min_x) ? cells[i].origin[0] : min_x; // hold on to min x
       min_y = (cells[i].origin[1] < min_y) ? cells[i].origin[1] : min_y; // hold on to min y
+      beam_ncells++;
     }
   }
-  beam_ncells_2 = 2*beam_ncells;
 
   beam_center[0] = temp_beam_center[0]/beam_ncells; // beam center x
   beam_center[1] = temp_beam_center[1]/beam_ncells; // beam center y
@@ -187,77 +186,79 @@ void shk_zernike_matrix(shkcell_t *cells,double *matrix_inv){
   printf("SHK: Rebuilding Zernike Matrix\n");
   printf("SHK: Beam center %f %f\n",beam_center[0],beam_center[1]);
   printf("SHK: Beam radius %f %f\n",beam_radius[0],beam_radius[1]);
+  printf("SHK: number of beam cells %d\n", beam_ncells);
 
   //multiplying vector is in slopes
   beam_radius_multiplier[0] = 1/(beam_radius[0]*SHK_PX_PITCH_UM);
   beam_radius_multiplier[1] = 1/(beam_radius[1]*SHK_PX_PITCH_UM);
 
+  printf("SHK: building the z matrix\n");
   double dz_dxdy[2*SHK_NCELLS*LOWFS_N_ZERNIKE] = {0};
   double x_1 = 0, x_2 = 0, x_3 = 0, x_4 = 0, x_5=0;
   double y_1 = 0, y_2 = 0, y_3 = 0, y_4 = 0, y_5=0;
   for(i=0; i<beam_ncells; i++) {
-    x_1 = (cells[beam_cell_index[beam_ncells]].origin[0] - beam_center[0])/beam_radius[0];
+    x_1 = (cells[beam_cell_index[i]].origin[0] - beam_center[0])/beam_radius[0];
     x_2 = pow(x_1,2);
     x_3 = pow(x_1,3);
     x_4 = pow(x_1,4);
     x_5 = pow(x_1,5);
-    y_1 = (cells[beam_cell_index[beam_ncells]].origin[1] - beam_center[1])/beam_radius[1];
+    y_1 = (cells[beam_cell_index[i]].origin[1] - beam_center[1])/beam_radius[1];
     y_2 = pow(y_1,2);
     y_3 = pow(y_1,3);
     y_4 = pow(y_1,4);
     y_5 = pow(y_1,5);
     //NOTE: This is hard coded for LOWFS_N_ZERNIKE == 24
-    dz_dxdy[i                   ] =      1.0 * (0)                                                                    *beam_radius_multiplier[0];
-    dz_dxdy[i+( 1*beam_ncells_2)] =      2.0 * (1)                                                                    *beam_radius_multiplier[0];
-    dz_dxdy[i+( 2*beam_ncells_2)] =      2.0 * (0)                                                                    *beam_radius_multiplier[0];
-    dz_dxdy[i+( 3*beam_ncells_2)] =  sqrt(3) * (4*x_1)                                                                *beam_radius_multiplier[0];
-    dz_dxdy[i+( 4*beam_ncells_2)] =  sqrt(6) * (2*y_1)                                                                *beam_radius_multiplier[0];
-    dz_dxdy[i+( 5*beam_ncells_2)] =  sqrt(6) * (2*x_1)                                                                *beam_radius_multiplier[0];
-    dz_dxdy[i+( 6*beam_ncells_2)] =  sqrt(8) * (9*x_2 + 3*y_2 - 2)                                                    *beam_radius_multiplier[0];
-    dz_dxdy[i+( 7*beam_ncells_2)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[0];
-    dz_dxdy[i+( 8*beam_ncells_2)] =  sqrt(8) * (3*x_2 - 3*y_2)                                                        *beam_radius_multiplier[0];
-    dz_dxdy[i+( 9*beam_ncells_2)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[0];
-    dz_dxdy[i+(10*beam_ncells_2)] = sqrt( 5) * (24*x_3 - 12*x_1 + 24*x_1*y_2)                                         *beam_radius_multiplier[0];
-    dz_dxdy[i+(11*beam_ncells_2)] = sqrt(10) * (16*x_3 - 6*x_1)                                                       *beam_radius_multiplier[0];
-    dz_dxdy[i+(12*beam_ncells_2)] = sqrt(10) * (24*x_2*y_1 - 6*y_1 + 8*y_3)                                           *beam_radius_multiplier[0];
-    dz_dxdy[i+(13*beam_ncells_2)] = sqrt(10) * (4*x_3 - 12*x_1*y_2)                                                   *beam_radius_multiplier[0];
-    dz_dxdy[i+(14*beam_ncells_2)] = sqrt(10) * (12*x_2*y_1 - 4*y_3)                                                   *beam_radius_multiplier[0];
-    dz_dxdy[i+(15*beam_ncells_2)] = sqrt(12) * (50*x_4 - 36*x_2 + 60*x_2*y_2 - 12*y_2 + 10*y_4 + 3)                   *beam_radius_multiplier[0];
-    dz_dxdy[i+(16*beam_ncells_2)] = sqrt(12) * (40*x_3*y_1 - 24*x_1*y_1 + 40*x_1*y_3)                                 *beam_radius_multiplier[0];
-    dz_dxdy[i+(17*beam_ncells_2)] = sqrt(12) * (25*x_4 - 12*x_2 - 30*x_2*y_2 + 12*y_2 - 15*y_4)                       *beam_radius_multiplier[0];
-    dz_dxdy[i+(18*beam_ncells_2)] = sqrt(12) * (60*x_3*y_1 - 24*x_1*y_1 + 20*x_1*y_3)                                 *beam_radius_multiplier[0];
-    dz_dxdy[i+(19*beam_ncells_2)] = sqrt(12) * (5*x_4 - 30*x_2*y_2 + 5*y_4)                                           *beam_radius_multiplier[0];
-    dz_dxdy[i+(20*beam_ncells_2)] = sqrt(12) * (20*x_3*y_1 - 20*x_1*y_3)                                              *beam_radius_multiplier[0];
-    dz_dxdy[i+(21*beam_ncells_2)] = sqrt( 7) * (120*x_5 - 120*x_3 + 240*x_3*y_2 + 24*x_1 - 120*x_1*y_2 + 120*x_1*y_4) *beam_radius_multiplier[0];
-    dz_dxdy[i+(22*beam_ncells_2)] = sqrt(14) * (90*x_5 + 60*x_3*y_2 - 80*x_3 + 12*x_1 - 30*x_1*y_4)                   *beam_radius_multiplier[0];
-    dz_dxdy[i+(23*beam_ncells_2)] = sqrt(14) * (150*x_4*y_1 + 180*x_2*y_3 - 120*x_2*y_1 + 12*y_1 - 40*y_3 + 30*y_5)   *beam_radius_multiplier[0];
+    dz_dxdy[i                 ] =      1.0 * (0)                                                                    *beam_radius_multiplier[0];
+    dz_dxdy[i+( 2*beam_ncells)] =      2.0 * (1)                                                                    *beam_radius_multiplier[0];
+    dz_dxdy[i+( 4*beam_ncells)] =      2.0 * (0)                                                                    *beam_radius_multiplier[0];
+    dz_dxdy[i+( 6*beam_ncells)] =  sqrt(3) * (4*x_1)                                                                *beam_radius_multiplier[0];
+    dz_dxdy[i+( 8*beam_ncells)] =  sqrt(6) * (2*y_1)                                                                *beam_radius_multiplier[0];
+    dz_dxdy[i+(10*beam_ncells)] =  sqrt(6) * (2*x_1)                                                                *beam_radius_multiplier[0];
+    dz_dxdy[i+(12*beam_ncells)] =  sqrt(8) * (9*x_2 + 3*y_2 - 2)                                                    *beam_radius_multiplier[0];
+    dz_dxdy[i+(14*beam_ncells)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[0];
+    dz_dxdy[i+(16*beam_ncells)] =  sqrt(8) * (3*x_2 - 3*y_2)                                                        *beam_radius_multiplier[0];
+    dz_dxdy[i+(18*beam_ncells)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[0];
+    dz_dxdy[i+(20*beam_ncells)] = sqrt( 5) * (24*x_3 - 12*x_1 + 24*x_1*y_2)                                         *beam_radius_multiplier[0];
+    dz_dxdy[i+(22*beam_ncells)] = sqrt(10) * (16*x_3 - 6*x_1)                                                       *beam_radius_multiplier[0];
+    dz_dxdy[i+(24*beam_ncells)] = sqrt(10) * (24*x_2*y_1 - 6*y_1 + 8*y_3)                                           *beam_radius_multiplier[0];
+    dz_dxdy[i+(26*beam_ncells)] = sqrt(10) * (4*x_3 - 12*x_1*y_2)                                                   *beam_radius_multiplier[0];
+    dz_dxdy[i+(28*beam_ncells)] = sqrt(10) * (12*x_2*y_1 - 4*y_3)                                                   *beam_radius_multiplier[0];
+    dz_dxdy[i+(30*beam_ncells)] = sqrt(12) * (50*x_4 - 36*x_2 + 60*x_2*y_2 - 12*y_2 + 10*y_4 + 3)                   *beam_radius_multiplier[0];
+    dz_dxdy[i+(32*beam_ncells)] = sqrt(12) * (40*x_3*y_1 - 24*x_1*y_1 + 40*x_1*y_3)                                 *beam_radius_multiplier[0];
+    dz_dxdy[i+(34*beam_ncells)] = sqrt(12) * (25*x_4 - 12*x_2 - 30*x_2*y_2 + 12*y_2 - 15*y_4)                       *beam_radius_multiplier[0];
+    dz_dxdy[i+(36*beam_ncells)] = sqrt(12) * (60*x_3*y_1 - 24*x_1*y_1 + 20*x_1*y_3)                                 *beam_radius_multiplier[0];
+    dz_dxdy[i+(38*beam_ncells)] = sqrt(12) * (5*x_4 - 30*x_2*y_2 + 5*y_4)                                           *beam_radius_multiplier[0];
+    dz_dxdy[i+(40*beam_ncells)] = sqrt(12) * (20*x_3*y_1 - 20*x_1*y_3)                                              *beam_radius_multiplier[0];
+    dz_dxdy[i+(42*beam_ncells)] = sqrt( 7) * (120*x_5 - 120*x_3 + 240*x_3*y_2 + 24*x_1 - 120*x_1*y_2 + 120*x_1*y_4) *beam_radius_multiplier[0];
+    dz_dxdy[i+(44*beam_ncells)] = sqrt(14) * (90*x_5 + 60*x_3*y_2 - 80*x_3 + 12*x_1 - 30*x_1*y_4)                   *beam_radius_multiplier[0];
+    dz_dxdy[i+(46*beam_ncells)] = sqrt(14) * (150*x_4*y_1 + 180*x_2*y_3 - 120*x_2*y_1 + 12*y_1 - 40*y_3 + 30*y_5)   *beam_radius_multiplier[0];
 
-    dz_dxdy[i+(24*beam_ncells_2)] =      1.0 * (0)                                                                    *beam_radius_multiplier[1];
-    dz_dxdy[i+(25*beam_ncells_2)] =      2.0 * (0)                                                                    *beam_radius_multiplier[1];
-    dz_dxdy[i+(26*beam_ncells_2)] =      2.0 * (1)                                                                    *beam_radius_multiplier[1];
-    dz_dxdy[i+(27*beam_ncells_2)] =  sqrt(3) * (4*y_1)                                                                *beam_radius_multiplier[1];
-    dz_dxdy[i+(28*beam_ncells_2)] =  sqrt(6) * (2*x_1)                                                                *beam_radius_multiplier[1];
-    dz_dxdy[i+(29*beam_ncells_2)] =  sqrt(6) * (-2*y_1)                                                               *beam_radius_multiplier[1];
-    dz_dxdy[i+(30*beam_ncells_2)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[1];
-    dz_dxdy[i+(31*beam_ncells_2)] =  sqrt(8) * (3*x_2 + 9*y_2 - 2)                                                    *beam_radius_multiplier[1];
-    dz_dxdy[i+(32*beam_ncells_2)] =  sqrt(8) * (-6*x_1*y_1)                                                           *beam_radius_multiplier[1];
-    dz_dxdy[i+(33*beam_ncells_2)] =  sqrt(8) * (3*x_2 - 3*y_2)                                                        *beam_radius_multiplier[1];
-    dz_dxdy[i+(34*beam_ncells_2)] = sqrt( 5) * (24*x_2*y_1 - 12*y_1 + 24*y_3)                                         *beam_radius_multiplier[1];
-    dz_dxdy[i+(35*beam_ncells_2)] = sqrt(10) * (6*y_1 - 16*y_3)                                                       *beam_radius_multiplier[1];
-    dz_dxdy[i+(36*beam_ncells_2)] = sqrt(10) * (8*x_3 - 6*x_1 + 24*x_1*y_2)                                           *beam_radius_multiplier[1];
-    dz_dxdy[i+(37*beam_ncells_2)] = sqrt(10) * (-12*x_2*y_1 + 4*y_3)                                                  *beam_radius_multiplier[1];
-    dz_dxdy[i+(38*beam_ncells_2)] = sqrt(10) * (4*x_3 - 12*x_1*y_2)                                                   *beam_radius_multiplier[1];
-    dz_dxdy[i+(39*beam_ncells_2)] = sqrt(12) * (40*x_3*y_1 -24*x_1*y_1 + 40*x_1*y_3)                                  *beam_radius_multiplier[1];
-    dz_dxdy[i+(40*beam_ncells_2)] = sqrt(12) * (10*x_4 - 12*x_2 + 60*x_2*y_2 - 36*y_2 + 50*y_4 + 3)                   *beam_radius_multiplier[1];
-    dz_dxdy[i+(41*beam_ncells_2)] = sqrt(12) * (-20*x_3*y_1 + 24*x_1*y_1 - 60*x_1*y_3)                                *beam_radius_multiplier[1];
-    dz_dxdy[i+(42*beam_ncells_2)] = sqrt(12) * (15*x_4 - 12*x_2 + 30*x_2*y_2 + 12*y_2 - 25*y_4)                       *beam_radius_multiplier[1];
-    dz_dxdy[i+(43*beam_ncells_2)] = sqrt(12) * (-20*x_3*y_1 + 20*x_1*y_3)                                             *beam_radius_multiplier[1];
-    dz_dxdy[i+(44*beam_ncells_2)] = sqrt(12) * (5*x_4 - 30*x_2*y_2 + 5*y_4)                                           *beam_radius_multiplier[1];
-    dz_dxdy[i+(45*beam_ncells_2)] = sqrt( 7) * (120*x_4*y_1 - 120*x_2*y_1 + 240*x_2*y_3 + 24*y_1 - 120*y_3 + 120*y_5) *beam_radius_multiplier[1];
-    dz_dxdy[i+(46*beam_ncells_2)] = sqrt(14) * (30*x_4*y_1 - 60*x_2*y_3 - 12*y_1 + 80*y_3 - 90*y_5)                   *beam_radius_multiplier[1];
-    dz_dxdy[i+(47*beam_ncells_2)] = sqrt(14) * (30*x_5 - 40*x_3 + 180*x_3*y_2 + 12*x_1 - 120*x_1*y_2 + 150*x_1*y_4)   *beam_radius_multiplier[1];
+    dz_dxdy[i+(   beam_ncells)] =      1.0 * (0)                                                                    *beam_radius_multiplier[1];
+    dz_dxdy[i+( 3*beam_ncells)] =      2.0 * (0)                                                                    *beam_radius_multiplier[1];
+    dz_dxdy[i+( 5*beam_ncells)] =      2.0 * (1)                                                                    *beam_radius_multiplier[1];
+    dz_dxdy[i+( 7*beam_ncells)] =  sqrt(3) * (4*y_1)                                                                *beam_radius_multiplier[1];
+    dz_dxdy[i+( 9*beam_ncells)] =  sqrt(6) * (2*x_1)                                                                *beam_radius_multiplier[1];
+    dz_dxdy[i+(11*beam_ncells)] =  sqrt(6) * (-2*y_1)                                                               *beam_radius_multiplier[1];
+    dz_dxdy[i+(13*beam_ncells)] =  sqrt(8) * (6*x_1*y_1)                                                            *beam_radius_multiplier[1];
+    dz_dxdy[i+(15*beam_ncells)] =  sqrt(8) * (3*x_2 + 9*y_2 - 2)                                                    *beam_radius_multiplier[1];
+    dz_dxdy[i+(17*beam_ncells)] =  sqrt(8) * (-6*x_1*y_1)                                                           *beam_radius_multiplier[1];
+    dz_dxdy[i+(19*beam_ncells)] =  sqrt(8) * (3*x_2 - 3*y_2)                                                        *beam_radius_multiplier[1];
+    dz_dxdy[i+(21*beam_ncells)] = sqrt( 5) * (24*x_2*y_1 - 12*y_1 + 24*y_3)                                         *beam_radius_multiplier[1];
+    dz_dxdy[i+(23*beam_ncells)] = sqrt(10) * (6*y_1 - 16*y_3)                                                       *beam_radius_multiplier[1];
+    dz_dxdy[i+(25*beam_ncells)] = sqrt(10) * (8*x_3 - 6*x_1 + 24*x_1*y_2)                                           *beam_radius_multiplier[1];
+    dz_dxdy[i+(27*beam_ncells)] = sqrt(10) * (-12*x_2*y_1 + 4*y_3)                                                  *beam_radius_multiplier[1];
+    dz_dxdy[i+(29*beam_ncells)] = sqrt(10) * (4*x_3 - 12*x_1*y_2)                                                   *beam_radius_multiplier[1];
+    dz_dxdy[i+(31*beam_ncells)] = sqrt(12) * (40*x_3*y_1 -24*x_1*y_1 + 40*x_1*y_3)                                  *beam_radius_multiplier[1];
+    dz_dxdy[i+(33*beam_ncells)] = sqrt(12) * (10*x_4 - 12*x_2 + 60*x_2*y_2 - 36*y_2 + 50*y_4 + 3)                   *beam_radius_multiplier[1];
+    dz_dxdy[i+(35*beam_ncells)] = sqrt(12) * (-20*x_3*y_1 + 24*x_1*y_1 - 60*x_1*y_3)                                *beam_radius_multiplier[1];
+    dz_dxdy[i+(37*beam_ncells)] = sqrt(12) * (15*x_4 - 12*x_2 + 30*x_2*y_2 + 12*y_2 - 25*y_4)                       *beam_radius_multiplier[1];
+    dz_dxdy[i+(39*beam_ncells)] = sqrt(12) * (-20*x_3*y_1 + 20*x_1*y_3)                                             *beam_radius_multiplier[1];
+    dz_dxdy[i+(41*beam_ncells)] = sqrt(12) * (5*x_4 - 30*x_2*y_2 + 5*y_4)                                           *beam_radius_multiplier[1];
+    dz_dxdy[i+(43*beam_ncells)] = sqrt( 7) * (120*x_4*y_1 - 120*x_2*y_1 + 240*x_2*y_3 + 24*y_1 - 120*y_3 + 120*y_5) *beam_radius_multiplier[1];
+    dz_dxdy[i+(45*beam_ncells)] = sqrt(14) * (30*x_4*y_1 - 60*x_2*y_3 - 12*y_1 + 80*y_3 - 90*y_5)                   *beam_radius_multiplier[1];
+    dz_dxdy[i+(47*beam_ncells)] = sqrt(14) * (30*x_5 - 40*x_3 + 180*x_3*y_2 + 12*x_1 - 120*x_1*y_2 + 150*x_1*y_4)   *beam_radius_multiplier[1];
   }
-
+  printf("SHK: inverting the z matrix\n");
   num_dgesvdi(dz_dxdy, matrix_inv, 2*beam_ncells, LOWFS_N_ZERNIKE);
 }
 
@@ -265,37 +266,44 @@ void shk_zernike_matrix(shkcell_t *cells,double *matrix_inv){
 /*                      SHK_ZERNIKE_FIT                       */
 /**************************************************************/
 void shk_zernike_fit(shkevent_t *shkevent){
-  int i, beam_ncells=0, beam_ncells_2=0,regen_matrix=0;
+  int i, beam_ncells=0, beam_ncells_2=0, regen_matrix=0;
   int beam_cell_index[SHK_NCELLS] = {0};
   static int beam_cell_used[SHK_NCELLS] = {0};
   const double px2slope = SHK_PX_PITCH_UM/SHK_FOCAL_LENGTH_UM;
   static double matrix_inv[SHK_NCELLS*LOWFS_N_ZERNIKE*2] = {0};
+  static double dphi_dxdy[2*SHK_NCELLS] = {0};
 
   for(i=0; i<SHK_NCELLS; i++) {
     if (shkevent->cells[i].beam_select) {
       //save the spot index
-      beam_ncells++;
       beam_cell_index[beam_ncells] = i;
       //check if we should regenerate the matrix
       if(!beam_cell_used[i]) regen_matrix=1;
       beam_cell_used[i]=1;
+      beam_ncells++;
     }
   }
-  
+  beam_ncells_2 = 2*beam_ncells;
 
   //generate the zernike matrix
-  if(regen_matrix) shk_zernike_matrix(shkevent->cells,matrix_inv);
-#if 0 
-  beam_ncells_2 = 2*beam_ncells;
-  double* dphi_dxdy = (double*)malloc(beam_ncells_2);
-  for(i=0; i<=beam_ncells; i++) {
-    // multiplying vector is in slopes
-    dphi_dxdy[i]             = shkevent->cells[beam_cell_index[beam_ncells]].deviation[0]*px2slope;
-    dphi_dxdy[i+beam_ncells] = shkevent->cells[beam_cell_index[beam_ncells]].deviation[1]*px2slope;
+  if(regen_matrix) {
+    shk_zernike_matrix(shkevent->cells, matrix_inv);
+    printf("SHK: matrix inverted\n");
   }
-  num_dgemv(matrix_inv, dphi_dxdy, shkevent->zernikes, LOWFS_N_ZERNIKE, beam_ncells_2);
-  free(dphi_dxdy);
-#endif
+
+  for(i=0; i<beam_ncells; i++) {
+    // multiplying vector is in slopes
+    dphi_dxdy[i]             = shkevent->cells[beam_cell_index[i]].deviation[0]*px2slope;
+    dphi_dxdy[i+beam_ncells] = shkevent->cells[beam_cell_index[i]].deviation[1]*px2slope;
+  }
+  num_dgemv(matrix_inv, dphi_dxdy, shkevent->zernikes, beam_ncells_2, LOWFS_N_ZERNIKE);
+  
+  shkevent->zernikes[0] = 0;
+  printf("SHK: Zernike Amplitudes : [ ");
+  for(i=0; i<LOWFS_N_ZERNIKE; i++)
+    printf("%.4ef, ", shkevent->zernikes[i]);
+  printf("\b\b ]\n");
+
 }
 
 /**************************************************************/
