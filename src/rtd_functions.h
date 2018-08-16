b2bc0ba27bb8af3e0995c0dc1f5bf7ea4afa01e6
Christopher Mendillo <christopher_mendillo@uml.edu>
2018-08-16 16:01:17 -0400

    Removed rtd_function.h install_isr function prototype. Left in by accident.

--- src/rtd_functions.h
+++ src/rtd_functions.h
@@ -5,7 +5,6 @@
 DM7820_Error rtd_open(unsigned long minor_number, DM7820_Board_Descriptor** p_p_rtd_board);
 DM7820_Error rtd_reset(DM7820_Board_Descriptor* p_rtd_board);
 DM7820_Error rtd_close(DM7820_Board_Descriptor* p_rtd_board);
-DM7820_Error rtd_install_isr(DM7820_Board_Descriptor* p_rtd_board, void *isr_pass);
 DM7820_Error rtd_alp_cleanup(DM7820_Board_Descriptor* p_rtd_board);
 DM7820_Error rtd_tlm_cleanup(DM7820_Board_Descriptor* p_rtd_board);
 DM7820_Error rtd_start_alp_clock(DM7820_Board_Descriptor* p_rtd_board);
