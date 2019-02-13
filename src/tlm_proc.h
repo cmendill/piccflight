921289dc2e88fffa290e3468fa2e0064912f0c0e
Christopher Mendillo <christopher_mendillo@uml.edu>
2019-02-13 15:00:27 -0500

    Reverted tlm_proc to previous version because it was somehow causing image rolling on the SHk camera.

--- src/tlm_proc.h
+++ src/tlm_proc.h
@@ -4,20 +4,33 @@
 /*************************************************
  * TLM Parameters
  *************************************************/
+
+//DATA ID
+#define TLM_SCI 0x11110000
+#define TLM_SHK 0x22220000
+#define TLM_LYT 0x33330000
+#define TLM_ACQ 0x44440000
+#define TLM_DM  0x55550000
+#define TLM_IWC 0x66660000
+
 //SYNCWORDS
 #define TLM_PRESYNC   0x12345678
 #define TLM_POSTSYNC  0xDEADBEEF
 
 //SEND SWITCHES
-#define SEND_SCIEVENT  1
-#define SEND_SHKPKT    1
-#define SEND_LYTPKT    1
-#define SEND_ACQEVENT  0
+#define SEND_SCI    0
+#define SEND_SHK    0
+#define SEND_LYT    0
+#define SEND_ACQ    0
+#define SEND_DM     0
+#define SEND_IWC    0
 
 //SAVE SWITCHES
-#define SAVE_SCIEVENT  1
-#define SAVE_SHKPKT    1
-#define SAVE_LYTPKT    1
-#define SAVE_ACQEVENT  0
+#define SAVE_SCI    0
+#define SAVE_SHK    0
+#define SAVE_LYT    0
+#define SAVE_ACQ    0
+#define SAVE_DM     0
+#define SAVE_IWC    0
 
 #endif
