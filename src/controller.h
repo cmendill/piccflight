7f52a8d08796f87ee5164a57b35ffd696e6d2618
Christopher Mendillo <christopher_mendillo@uml.edu>
2021-11-17 15:03:51 -0500

    Updated SCI_SIM_MAX

--- src/controller.h
+++ src/controller.h
@@ -483,7 +483,7 @@ enum bufids {BUFFER_SCIEVENT, BUFFER_SHKEVENT,
 #define SCI_TEC_SETPOINT_MAX   35 //C
 #define SCI_EFC_GAIN         -0.5 //EFC gain
 #define SCI_SATURATION      65535 //SCI saturation
-#define SCI_SIM_MAX         {0.3} //[SCI_NBANDS] Maximum pixel value of unocculted image simulation (for field normalization)
+#define SCI_SIM_MAX         {0.0766} //[SCI_NBANDS] Maximum pixel value of unocculted image simulation (for field normalization)
 #define SCI_SCALE_DEFAULT {6.19e-9}//[SCI_NBANDS] Default image normalization for field calculation
 
 /*************************************************
