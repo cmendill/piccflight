b1e1c141a55ab7855ce213ddef3e190b396b0f5d
Christopher Mendillo <christopher_mendillo@uml.edu>
2022-08-30 12:28:27 -0400

    Packet version 48.

--- src/controller.h
+++ src/controller.h
@@ -1035,7 +1035,7 @@ typedef struct optmode_struct{
 /*************************************************
  * Packet Header
  *************************************************/
-#define PICC_PKT_VERSION     47  //packet version number
+#define PICC_PKT_VERSION     48  //packet version number
 typedef struct pkthed_struct{
   uint16  version;       //packet version number
   uint16  type;          //packet ID word
